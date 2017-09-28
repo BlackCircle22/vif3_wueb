b587511b7f8cb0a644047d68e8eeb270faa7710c
Julius S <schamonj@gmail.com>
2017-09-28 18:46:10 +0200

    Add files via upload

--- src/wueb/Operation.java
+++ src/wueb/Operation.java
@@ -1,23 +1,22 @@
-package wueb;
 public class Operation {
 
-    public Double umsatzsteuerBerechnen(double bruttoBetrag, double steuerProzent) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Double umsatzsteuerBerechnen(Double bruttoBetrag, Double steuerProzent) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        double e1 = 100 + steuerProzent;
+        System.out.println(e1);
+        double e2 = bruttoBetrag /e1;
+        System.out.println(e2);
+        double e3 = e2 * steuerProzent;
+        return e3;
     }
 
-    public Double nettoBerechnen(double bruttoBetrag, double steuerProzent) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Double nettoBerechnen(Double bruttoBetrag, Double steuerProzent) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        return bruttoBetrag + (bruttoBetrag * (steuerProzent / 100));
     }
 
-    public Double summeUeberschuss(double einnahmen, double ausgaben) {
-        
-        double summeUeberschuss = einnahmen - ausgaben;
-        
-        return summeUeberschuss;
-          
-    }
-
-    String summeUeberschuss() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Double summeUeberschuss(Double einnahme, Double ausgaben) {
+        //throw new UnsupportedOperationException("Not supported yet.");
+        return einnahme - ausgaben;
     }
 }
